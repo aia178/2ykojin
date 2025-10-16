69e58615f48c4a8a57e032501397a952ea0d809f
fujimori <2240195@ecc.ac.jp>
2025-10-16 13:49:54 +0900

    no message

---
+++ findjob/src/main.cpp
@@ -0,0 +1,34 @@
+#include <WiFi.h>
+
+const char* ssid = "ECCcomp3"; //ssid
+const char* password = "4Emah5LdS"; //password
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(10);
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
