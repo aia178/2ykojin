25456ebd7ad1f532b4a757edc846630997c79d96
fujimori <2240195@ecc.ac.jp>
2025-11-17 11:05:26 +0900

    no message

--- findjob/src/main.cpp
+++ findjob/src/main.cpp
@@ -43,20 +43,87 @@ void setup()
 
 void loop()
 {
-  // シリアルから入力があったら100円送信テスト
-    if (Serial.available() > 0) {
-        Serial.read(); // 入力を消費
-        
-        Serial.println("--- 送金テスト開始 ---");
-        bool success = sendDeposit(100, "test-goal-id", "テスト目標");
-        
-        if (success) {
-            Serial.println("✅ 送信成功！");
-        } else {
-            Serial.println("❌ 送信失敗");
-        }
-        Serial.println("--- テスト終了 ---");
+    float weight = getWeight();
+    int amount = returnAmount(weight);
+
+    if (amount > 0)
+    {
+        Serial.print("判定金額: ");
+        Serial.print(amount);
+        Serial.println(" 円");
+    }else {
+        Serial.println("該当しない硬貨です！");
     }
     
+
+
+    sendFirestore(amount);
+  
     delay(100);
+}
+
+// Firestoreに送金データを送る
+bool sendFirestore(int amount){
+    
+    
+    Serial.print("送金金額: ");
+    Serial.print(amount);
+    Serial.println(" 円");
+
+    bool success = sendDeposit(amount, "test-goal-id", "テスト目標");
+        
+    if (success) {
+        Serial.println("送信成功！");
+    } else {
+        Serial.println("送信失敗");
+    }
+
+    return success;
+    
+}
+
+
+// 重さ取ってきて表示する
+float getWeight(){
+    float weight = scale.get_units();
+    Serial.print("Weight: ");
+    Serial.print(weight);
+    Serial.println(" g");
+
+    return weight;
+}
+
+int returnAmount(float weight) {
+    
+    if (weight >= 0.6f && weight < 1.3f) {
+        return 1;   // 1円
+    }
+
+    
+    if (weight >= 3.0f && weight < 3.6f) {
+        return 5;   // 5円
+    }
+
+    
+    if (weight >= 3.6f && weight < 3.9f) {
+        return 50;
+    }
+
+    
+    if (weight >= 3.9f && weight < 4.35f) {
+        return 10;
+    }
+
+    
+    if (weight >= 4.35f && weight < 4.9f) {
+        return 100;
+    }
+
+    
+    if (weight >= 6.6f && weight < 7.6f) {
+        return 500; // 500円
+    }
+
+    //該当なし
+    return 0;
 }
